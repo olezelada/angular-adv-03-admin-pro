9eb88be78b304356c02d00729f7d3d73b2547cbd
ozelada <olegario.zelada@dharbor.com>
2023-06-04 19:42:59 -0400

    second version already routes completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {PagesModule} from "./pages/pages.module";
+import {AuthModule} from "./auth/auth.module";
+
+import {AppComponent} from './app.component';
+import {NopagesfoundComponent} from './nopagesfound/nopagesfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagesfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/pages/pages.routing.ts
@@ -0,0 +1,26 @@
+import {RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+import {PagesComponent} from "./pages.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {ProgressComponent} from "./progress/progress.component";
+import {GraphicOneComponent} from "./graphic-one/graphic-one.component";
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: PagesComponent,
+    children: [
+      {path: '', component: DashboardComponent},
+      {path: 'progress', component: ProgressComponent},
+      {path: 'graphic-one', component: GraphicOneComponent}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PagesRoutingModule {
+
+}
